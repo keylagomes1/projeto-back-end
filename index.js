53a0ff4f128071d6f714c10ead070a2460c1b09a
keyla <keylagomess06@gmail.com>
2025-06-16 14:52:56 -0300

    teste

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const { PrismaClient } = require('@prisma/client');
 const userRoutes = require('./routes/user');
+const produtosRoutes = require('./routes/produtos');
 
 dotenv.config();
 const prisma = new PrismaClient();
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/users', userRoutes);
+app.use('/produtos', produtosRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ routes/produtos.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const router = express.Router();
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+// Criar usuário
+router.post('/', async (req, res) => {
+    const { nome, slug, use_in_menu, stock , descricao, preco, preco_com_desconto    } = req.body;
+    try {
+        const produtos = await prisma.produtos.create({
+            data: { nome, slug, use_in_menu,  stock , descricao, preco, preco_com_desconto   }
+        });
+        res.json(produtos);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+// Listar usuários
+router.get('/', async (req, res) => {
+    const produtos = await prisma.produtos.findMany();
+    res.json(produtos);
+});
+// Listar por ID
+router.get('/:id', async (req, res) => {
+  const id = Number(req.params.id);
+  const produtos = await prisma.produtos.findUnique({ where: { id } });
+  if (!produtos) return res.status(404).json({ error: 'Produto não encontrado' });
+  res.json(produtos);
+});
+
+
+// Atualizar usuário
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { nome, slug, use_in_menu  } = req.body;
+    try {
+        const produtos = await prisma.produtos.update({
+            where: { id: parseInt(id) },
+            data: { nome, slug, use_in_menu,  stock , descricao, preco, preco_com_desconto   }
+        });
+        res.json(produtos);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+// atualizar com Patch
+router.patch('/:id', async (req, res) => {
+  const id = Number(req.params.id);
+  const updatedProdutos = await prisma.produtos.update({
+    where: { id },
+    data: req.body
+  });
+  res.json(updatedProdutos);
+});
+
+
+// Deletar usuário
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        await prisma.produtos.delete({
+            where: { id: parseInt(id) }
+        });
+        res.json({ message: 'Produto deletado' });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+module.exports = router;
